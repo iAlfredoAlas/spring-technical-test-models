e8d3397e29c59e6838b19f2b839bd7caa519eee9
Alfredo Alas <alfredo.alas14@gmail.com>
2024-03-24 13:42:41 -0600

    first commit

---
+++ src/test/java/com/example/spring/technical/test/models/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.technical.test.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
